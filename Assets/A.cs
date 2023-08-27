adf6323dffbeced2f8bc46e5f508c0145b502d35
jelly0211 <ssc7347@gmail.com>
2023-08-27 13:10:08 +0900

    초기 프로젝트 파일 업로드

---
+++ Assets/A.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       print("sdfsdfsdf");
+        Debug.Log("sdfsddfsd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
